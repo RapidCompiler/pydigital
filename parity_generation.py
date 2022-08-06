6da8484a7393c6fbfb5ef93c8e41baa968e66635
Sanjay <sanjusuresh2001@gmail.com>
2022-08-07 01:06:40 +0530

    Refactored parity functionality

--- parity_generation.py
+++ parity_generation.py
@@ -1,149 +1,180 @@
+# Encodes an array of binary numbers using LRC
 def EncodeLRC(arr):
-    # print(arr)
+    arrElemLen = arrElemLenCheck(arr)
+    if not arrElemLen[0]:
+        print("ENTERED DATA HAS INCONSISTENT NUMBER OF BITS FOR LRC")
+        raise ValueError("Entered data has inconsistent number of bits for LRC. All data strings must contain the same number of bits")
+
     lrc_parity = ""
-    for j in range(len(arr[0])):
-        parity_check = ''
-        for i in range(len(arr)):
-            parity_check += arr[i][j]
-        lrc_parity += str(parity_check.count('1') % 2)
+    for j in range(arrElemLen[1]):
+        lrc_parity += str(parityCalculator(arr, j))
+
+    print([lrc_parity, " ".join(arr) + " " + lrc_parity])
     return [lrc_parity, " ".join(arr) + " " + lrc_parity]
 
+# Encodes an array of binary numbers using VRC
+def EncodeVRC(arr, sep=None):
+    # Check if all elements of same size
+    arrElemLen = arrElemLenCheck(arr)
+    if not arrElemLen[0]:
+        print("ENTERED DATA HAS INCONSISTENT NUMBER OF BITS FOR VRC")
+        raise ValueError("Entered data has inconsistent number of bits for VRC. All data strings must contain the same number of bits")
 
-def EncodeVRC(arr):
-
-    arr = list(map(parseItemEncodeVRC, arr))
+    # Calculate parity for each data string and append to item
+    arr = list(map(parityCalculator, arr))
+    
+    # Getting the VRC bits alone
     vrc_parity = "".join([item[-1] for item in arr])
 
+    # Returning the final VRC transmitted string and VRC bits
     return [vrc_parity, " ".join(arr)]
 
-
-def EncodeCRC(key, dataString):
+# Encodes a dataString using CRC with a key
+def EncodeCRC(dataStream, key):
     dataToAppend = '0' * (len(key) - 1)
-    dividend = dataString + dataToAppend
+    dividend = dataStream + dataToAppend
     remainder = mod2div(dividend, key)
-    return [remainder, dataString + remainder]
+    return [remainder, dataStream + remainder]
 
-
+# Encodes a array of binary numbers using Checksum technique
 def EncodeChecksum(data1):
-    # print("THIS IS DATA",data1)
+    print(data1)
     data = data1.copy()
-    # data = list(map(int, data))
+    print(data)
     remainderLen = len(data[0])
+    
     while len(data) != 1:
+        # Using an array as a stack
         firstElem = data.pop(0)
         secondElem = data.pop(0)
         maxLen = max(len(firstElem), len(secondElem))
+
+        # Converting binary number to decimal integer
         firstNum = int(firstElem, 2)
         secondNum = int(secondElem, 2)
-        # print(firstNum, secondNum)
+
+        # Summing up the numbers and converting sum to binary
         sum = firstNum + secondNum
-        # print(sum)
         binSum = bin(sum)
+
+        # DONT UNDERSTAND THE BELOW SECTION OF CODE
         if len(binSum) - 2 != maxLen:
-            # print("THIS IS LEN : ",len(binSum), maxLen)
             binSum = bin(int(binSum[2:-maxLen], 2) + int(binSum[-maxLen:], 2))
             
         data.insert(0, binSum[2:].zfill(remainderLen))
-    # print(data)
+
     checksum = "".join(list(map(lambda x: '1' if x == '0' else '0', data[0])))
-    # print(checksum)
     return [checksum, " ".join(data1) + " " + checksum]
 
-# def DecodeLRC(arr):
-#     lrc_parity = EncodeLRC(arr[:-1])[0]
-#     # print(lrc_parity, arr[-1])
-#     if arr[-1] == lrc_parity: return "THE DATA TRANSMITTED IS CORRECT"
-#     return "THERE IS AN ERROR IN THE DATA"
-    
-    
-# def DecodeVRC(arr):
-#     vrc_parity_arr = "".join([dataString[-1] for dataString in arr])
-#     newArr = [dataString[:-1] for dataString in arr]
-#     vrc_parity = EncodeVRC(newArr)[0]
-#     if vrc_parity_arr == vrc_parity:
-#         return "THE DATA TRANSMITTED IS CORRECT"
-#     return "THERE IS AN ERROR IN THE DATA"
+# Decodes an LRC binary dataStream given the number of data strings and the number of bits per data string
+def DecodeLRC(dataStream, bits, bytes, retType="text"):
+    # Create array of data strings from dataStream
+    arr = arrGen4VRCLRCChecksum(dataStream, bits, bytes)
 
-
-# def DecodeCRC(key, dataString):
-#     remainder = mod2div(dataString, key)
-#     if not remainder.count('1'): return "THE DATA TRANSMITTED IS CORRECT"
-#     return "THERE IS AN ERROR IN THE DATA"
-
-
-# def DecodeChecksum(arr):
-#     checksum = EncodeChecksum(arr)
-#     # print(checksum)
-#     if not checksum[0].count('1'): return "THE DATA TRANSMITTED IS CORRECT"
-#     return "THERE IS AN ERROR IN THE DATA"
-#     pass
-
-def DecodeLRC(arr):
+    # No need to calculate LRC bit string here because it is already available in the last element of arr
+    # Calculating LRC parity using EncodeLRC function
     lrc_parity = EncodeLRC(arr[:-1])[0]
-    # print(lrc_parity, arr[-1])
+
+    # Checking if parity bits are correct
     if arr[-1] == lrc_parity:
-        print("DATA TRANSMITTED : "," ".join(arr[:-1]))
+        print("DATA TRANSMITTED : ", " ".join(arr[:-1]))
         return "THE DATA TRANSMITTED IS CORRECT"
+    
     return "THERE IS AN ERROR IN THE DATA"
     
-    
-def DecodeVRC(arr):
+# Decodes a VRC binary dataStream given the number of data strings and the number of bits per data string
+def DecodeVRC(dataStream, bits, bytes):
+    # Create array of data strings from dataStream
+    arr = arrGen4VRCLRCChecksum(dataStream, bits, bytes, True)
+    print(arr)
+    # Create VRC bit string
     vrc_parity_arr = "".join([dataString[-1] for dataString in arr])
-    newArr = [dataString[:-1] for dataString in arr]
-    vrc_parity = EncodeVRC(newArr)[0]
+    dataArr = [dataString[:-1] for dataString in arr]
+
+    # Calculating VRC parity using EncodeVRC function
+    vrc_parity = EncodeVRC(dataArr)[0]
+
+    # Checking if parity bits are correct
     if vrc_parity_arr == vrc_parity:
-        print("DATA TRANSMITTED : "," ".join(newArr))
+        print("DATA TRANSMITTED : "," ".join(dataArr))
         return "THE DATA TRANSMITTED IS CORRECT"
+
     return "THERE IS AN ERROR IN THE DATA"
 
-
-def DecodeCRC(key, dataString):
-    remainder = mod2div(dataString, key)
+# Decodes a CRC binary dataStream given the number of data strings and the number of bits per data string
+def DecodeCRC(dataStream, key):
+    remainder = mod2div(dataStream, key)
     if not remainder.count('1'):
-        print("DATA TRANSMITTED : ","".join(dataString[:-(len(key)-1)]))
+        print("DATA TRANSMITTED : ","".join(dataStream[:-(len(key)-1)]))
         return "THE DATA TRANSMITTED IS CORRECT"
     return "THERE IS AN ERROR IN THE DATA"
 
+# Decodes a Checksum binary dataStream given the number of data strings and the number of bits per data string
+def DecodeChecksum(dataStream, bits, bytes):
 
-def DecodeChecksum(arr):
+    arr = arrGen4VRCLRCChecksum(dataStream, bits, bytes)
+
     checksum = EncodeChecksum(arr)
-    # print(checksum)
     if not checksum[0].count('1'):
         print("DATA TRANSMITTED : "," ".join(arr[:-1]))
         return "THE DATA TRANSMITTED IS CORRECT"
     return "THERE IS AN ERROR IN THE DATA"
-    pass
 
+# Generate array for VRC and Checksum decoding
+def arrGen4VRCLRCChecksum(dataStream, bits, bytes, flag=False):
 
-def parseItemEncodeVRC(item):
-    parity_bit = str(item.count('1') % 2)
-    item += parity_bit
-    return item
+    arr = []
 
+    pointer = 0
+    upperPointer = bytes if flag else bytes + 1
+    while pointer + 1 <= upperPointer:
+        newElem = dataStream[(pointer) * (bits + 1):(pointer + 1) * (bits + 1)] if flag else dataStream[pointer * bits:(pointer + 1) * bits]
+        arr.append(newElem)
+        pointer += 1
 
+    return arr
+
+# Checks if all elements of an array have the same length
+def arrElemLenCheck(arr):
+    # Retrieving a list of array element sizes.
+    data_lens = set(map(len, arr))
+
+    # Set length must be one as there must be only one size
+    if len(data_lens) > 1:
+        return [False]
+
+    # Returning the length of the data streams
+    return [True, data_lens.pop()]
+
+# Calculates the LRC and VRC parity bits
+def parityCalculator(item, j=None):
+    # j variable is used for column number in LRC
+    parity_bit = 0
+    
+    for i in range(len(item)):
+        newItem = int(item[i][j]) if j != None else int(item[i])
+        parity_bit = parity_bit ^ newItem
+
+    if j == None:
+        return item + str(parity_bit)
+    return parity_bit
+
+# Outputs the xor of two binary data strings A and B
 def xor(a,b):
     result = []
 
     for i in range(1,len(b)):
-        if a[i] == b[i]:
-            result.append('0')
-        else:
-            result.append('1')
-    val = "".join(result)
-    # print("THIS IS XOR : ", a, b, val)
-    
-    return val
-    #return val.lstrip('0')
+        result.append(str(int(a[i]) ^ int(b[i])))
 
+    return "".join(result)
 
+# Modulo 2 division function used for CRC bits calculation
 def mod2div(dividend, divisor):
-    # print(dividend, divisor)
     pick = len(divisor)
 
     tmp = dividend[:pick]
 
     while pick < len(dividend):
-        # print("THIS IS TEMP : ", tmp)
         if tmp[0] == '1':
             tmp = xor(divisor, tmp) + dividend[pick]
 
@@ -160,107 +191,51 @@ def mod2div(dividend, divisor):
 
     return tmp
 
-
-def TX():
-    pass
-
-
-def RX():
-    pass
-
-
+# Main function
 def main():
-        
-    input_validation = True
-    input_count = False
-
     # RX or TX Choice
     choice = int(input("DO YOU WANT TO TRANSMIT OR RECEIVE DATA\n1. TRANSMIT\n2. RECEIVE\n\nYOUR CHOICE : "))
     print()
 
     # Error Detection Algorithm Choice
-    while input_validation:
-        option = input("WHICH ERROR DETECTION ALGORITHM DO YOU WANT TO USE?\n1. LRC\n2. VRC\n3. CRC\n4. CHECKSUM\n\nYOUR CHOICE : ")
-        print()
-        if option == '1' or option == '2' or option == '3' or option == '4': input_validation = not input_validation
-        else: print("NON-EXISTENT OPTION! CHOOSE AGAIN")
+    option = input("WHICH ERROR DETECTION ALGORITHM DO YOU WANT TO USE?\n1. LRC\n2. VRC\n3. CRC\n4. CHECKSUM\n\nYOUR CHOICE : ")
+    print()
 
-    # Pre-defining input_count variable for RX and also for CRC and Checksum
-    if option == '3' or choice == 2: input_count = 1
-
-    # Data Input Count Block for TX (Applicable only to LRC and VRC)
-    if not input_count:
-        input_count = int(input("HOW MANY DATA STRINGS DO YOU WANT TO PROCESS? "))
-        print()
-
-    # Data Input Block for Transmission
-    if choice == 1:
-        data = []
-        for i in range(input_count):
-            a = input("ENTER INPUT DATA " + str(i+1) + " : ")
-            print()
-            data.append(a)
-
-        data_lens = [len(data_string) for data_string in data]
-        if len(set(data_lens)) > 1:
-            print("ENTERED DATA HAS INCONSISTENT NUMBER OF BITS FOR LRC")
-            input_validation = True
-            
     # Data Input Block for Reception
-    elif choice == 2:
+    if choice == 2:
         dataStream = input("ENTER THE RECEIVED DATA STREAM : ")
         if option == '1' or option == '2' or option == '4':
             bytes = int(input("ENTER THE NUMBER OF DATA STRINGS IN DATA STREAM : "))
             bits = int(input("ENTER THE NUMBER OF BITS IN EACH DATA STRING : "))
-        if option == '2' and (len(dataStream) - int(bytes)) % bits or option == '1' and len(dataStream) != (bytes * bits) + bits: print("\nTHERE IS AN INCONSISTENCY IN THE DATA ENTERED\n", len(dataStream) - 1, bits); return
-
+        if (option == '2' and (len(dataStream) - int(bytes)) % bits) or (option == '1' and len(dataStream) != (bytes * bits) + bits): print("\nTHERE IS AN INCONSISTENCY IN THE DATA ENTERED\n", len(dataStream) - 1, bits); return
     
     print()
+
 
     # Encoding and Decoding Block Starts Here
     print("----------------------------------------------")
     if option == '1':
         # LRC TX
         if choice == 1:
-
-            lrc = EncodeLRC(data)
-            # print("----------------------------------------------")
+            lrc = EncodeLRC(['10110', '10100'])
             print("LRC :", lrc[0])
             print("DATA TRANSMITTED :", lrc[1])
-            # print("----------------------------------------------")
+
         # LRC RX
         else:
-            # Code to parse received data
-            data = []
-            pointer = 0
-            while pointer + 1 <= bytes + 1:
-                # print(pointer * bits, pointer + 1 * bits)
-                dataString = dataStream[pointer * bits:(pointer + 1) * bits]
-                data.append(dataString)
-                pointer += 1
-            
-            lrc = DecodeLRC(data)
+            lrc = DecodeLRC(dataStream, bits, bytes)
             print(lrc)
             
     if option == '2':
         # VRC TX
         if choice == 1:
-            vrc = EncodeVRC(data)
-            # print("----------------------------------------------")
+            vrc = EncodeVRC(['10110', '10100'])
             print("VRC :", vrc[0])
             print("DATA TRANSMITTED :", vrc[1])
-            # print("----------------------------------------------")
         
         # VRC RX
         else:
-            # Code to parse received data
-            data = []
-            pointer = 0
-            while pointer + 1 <= bytes:
-                dataString = dataStream[(pointer) * (bits + 1):(pointer + 1) * (bits + 1)]
-                data.append(dataString)
-                pointer += 1
-            vrc = DecodeVRC(data)
+            vrc = DecodeVRC(dataStream, bits, bytes)
             print(vrc)
     
     if option == '3':
@@ -268,48 +243,29 @@ def main():
         print()
         # CRC TX
         if choice == 1:
-            crc = EncodeCRC(key, data[0])
-            # print("----------------------------------------------")
+            crc = EncodeCRC('100100', '1101')
             print("CRC :", crc[0])
             print("DATA TRANSMITTED :", crc[1])
-            # print("----------------------------------------------")
 
         # CRC RX
         else:
-            # Code to parse received data
-            crc = DecodeCRC(key, dataStream)
-            # print("----------------------------------------------")
+            crc = DecodeCRC('100100001', '1101')
             print(crc)
 
     if option == '4':
+        # Checksum TX
         if choice == 1:
-            checksum = EncodeChecksum(data)
+            checksum = EncodeChecksum(['11001100', '10101010', '11110000', '11000011'])
             print("CHECKSUM :", checksum[0])
             print("DATA TRANSMITTED :", checksum[1])
 
+        # Checksum RX
         else:
-            data = []
-            pointer = 0
-            while pointer + 1 <= bytes + 1:
-                dataString = dataStream[pointer * bits: (pointer + 1) * bits]
-                data.append(dataString)
-                pointer += 1
-            # print(data)
-            checksum = DecodeChecksum(data)
+            checksum = DecodeChecksum(dataStream, bits, bytes)
             print(checksum)
 
     print("----------------------------------------------")
     # Encoding and Decoding Block Ends Here
-        
-        # print()
-        # option = input("TO EXIT,\nPRESS F TO KILL THE PROGRAM AND PAY RESPECTS f/(anything other than f): ")
-
-        # if option in ['F', 'f']:
-        #     print("PAYING RESPECTS....\nRESPECTS PAID")
-        #     break
-        
-        # input_count = False
-        # input_validation = True
 
 main()
 
